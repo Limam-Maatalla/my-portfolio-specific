ade59e758eda8e37a10d8b812df15ec423133c5f
Limam-Maatalla <limammaatalla526@gmail.com>
2026-02-23 09:20:24 +0000

    potfolio add

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let btn = document.querySelector('#menu-btn')
-let navbar0 = document.querySelector('header nav')
+let btn0 = document.getElementById('menu-btn')
+let navbar0 = document.querySelector('nav')
 
 
-btn.onclick = () =>{
-    btn.classList.toggle('fa-times')
+btn0.addEventListener("click", function(){
+    btn0.classList.toggle('fa-bars')
+    btn0.classList.toggle('fa-times')
     navbar0.classList.toggle('active')
-}
+})
+    
+    
